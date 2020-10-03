f26cac5453de69c5a067eb76d85c40c7e439a183
June Burkle <jonathan.k.burkle@gmail.com>
2020-10-03 00:53:09 -0400

    Implement dash projectile
    - Add new dash projectile class
    - Add dash detonate and teleport effect
    - Bind dash projectile to character

---
+++ Source/ActionRogueLike/Private/SDashProjectile.cpp
@@ -0,0 +1,77 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SDashProjectile.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/SphereComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+// Sets default values
+ASDashProjectile::ASDashProjectile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SphereComp = CreateDefaultSubobject<USphereComponent>("SphereComp");
+	SphereComp->SetCollisionProfileName("Projectile");
+	RootComponent = SphereComp;
+	SphereComp->OnComponentHit.AddDynamic(this, &ASDashProjectile::OnHit);	// set up a notification for when this component hits something blocking
+
+	EffectComp = CreateDefaultSubobject<UParticleSystemComponent>("EffectComp");
+	EffectComp->SetupAttachment(SphereComp);
+
+	MovementComp = CreateDefaultSubobject<UProjectileMovementComponent>("MovementComp");
+	MovementComp->ProjectileGravityScale = 0.0f;
+	MovementComp->InitialSpeed = 6000.0f;
+	MovementComp->bRotationFollowsVelocity = true;
+	MovementComp->bInitialVelocityInLocalSpace = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASDashProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetWorldTimerManager().SetTimer(TimerHandle_Detonate, this, &ASDashProjectile::Detonate, 0.2f);
+}
+
+// Called every frame
+void ASDashProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ASDashProjectile::Detonate()
+{
+	GetWorldTimerManager().ClearTimer(TimerHandle_Detonate); // in case it was called for another reason
+
+	UGameplayStatics::SpawnEmitterAtLocation(this, DetonateEffect, GetActorLocation(), GetActorRotation());
+
+	EffectComp->DeactivateSystem();
+	MovementComp->StopMovementImmediately(); // stop so we don't teleport further than we want
+	SetActorEnableCollision(false);
+	
+	FTimerHandle TimerHandle_Teleport;
+	GetWorldTimerManager().SetTimer(TimerHandle_Teleport, this, &ASDashProjectile::Teleport, 0.2f);
+
+}
+
+void ASDashProjectile::Teleport()
+{
+	AActor* PlayerTeleport = GetInstigator();
+
+	PlayerTeleport->TeleportTo(GetActorLocation(), PlayerTeleport->GetActorRotation(), false, false);
+}
+
+void ASDashProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	// When we hit something trigger the detonation
+	if ((OtherActor != NULL) && (OtherActor != this) && (OtherComp != NULL) && OtherComp->IsSimulatingPhysics())
+	{
+		Detonate();
+	}
+}
+
