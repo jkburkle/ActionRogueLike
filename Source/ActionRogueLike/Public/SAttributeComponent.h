a5cddcfd1e293019104392d4861e3fdc56f75036
June Burkle <jonathan.k.burkle@gmail.com>
2020-10-08 19:27:15 -0400

    Add player health widget
    - Widget updates based off of new change event triggering
    - Very basic UI implementation (no animation or shader)

--- Source/ActionRogueLike/Public/SAttributeComponent.h
+++ Source/ActionRogueLike/Public/SAttributeComponent.h
@@ -7,6 +7,9 @@
 #include "SAttributeComponent.generated.h"
 
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams(FOnHealthChanged, AActor*, InstigatorActor, USAttributeComponent*, OwningComp, float, NewHealth, float, Delta);
+
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class ACTIONROGUELIKE_API USAttributeComponent : public UActorComponent
 {
@@ -22,6 +25,9 @@ protected:
 	float Health;
 
 public:	
+
+	UPROPERTY(BlueprintAssignable)
+	FOnHealthChanged OnHealthChanged;
 
 	UFUNCTION(BlueprintCallable, Category="Attributes")
 	bool ApplyHealthChange(float Delta);
