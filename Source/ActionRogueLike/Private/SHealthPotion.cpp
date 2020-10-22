9dc25240290031ef845789a33eeb787aa3ccfb6f
June Burkle <jonathan.k.burkle@gmail.com>
2020-10-22 18:37:24 -0400

    Create powerup base class
    - Add powerup base class
    - Change potion behavior to heal to full health (not 20 HP)

--- Source/ActionRogueLike/Private/SHealthPotion.cpp
+++ Source/ActionRogueLike/Private/SHealthPotion.cpp
@@ -2,69 +2,33 @@
 
 
 #include "SHealthPotion.h"
-#include "Components/SphereComponent.h"
 #include "SAttributeComponent.h"
 
 
-// Sets default values
 ASHealthPotion::ASHealthPotion()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
-	SphereComp = CreateDefaultSubobject<USphereComponent>("SphereComp");
-	RootComponent = SphereComp;
-
 	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>("MeshComp");
+	// Disable collision, instead we use SphereComp to handle interaction queries
 	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 	MeshComp->SetupAttachment(RootComponent);
-
-	RespawnTime = 10.0f;
-
 }
 
-// Called when the game starts or when spawned
-void ASHealthPotion::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void ASHealthPotion::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-void ASHealthPotion::Activate()
-{
-	SetActorEnableCollision(true);
-	RootComponent->SetVisibility(true, true);
-}
-
-void ASHealthPotion::Deactivate()
-{
-	SetActorEnableCollision(false);
-	RootComponent->SetVisibility(false, true);
-
-	// wait
-	FTimerHandle TimerHandle_RespawnTimer;
-	GetWorldTimerManager().SetTimer(TimerHandle_RespawnTimer, this, &ASHealthPotion::Activate, RespawnTime);
-}
 
 void ASHealthPotion::Interact_Implementation(APawn* InstigatorPawn)
 {
-	if (InstigatorPawn)
+	if (!ensure(InstigatorPawn))
 	{
-		USAttributeComponent* AttributeComp = Cast<USAttributeComponent>(InstigatorPawn->GetComponentByClass(USAttributeComponent::StaticClass()));
-		if (AttributeComp && !AttributeComp->IsFullHealth()) // check for max health
+		return;
+	}
+
+	USAttributeComponent* AttributeComp = USAttributeComponent::GetAttributes(InstigatorPawn);
+	// Check if not already at max health
+	if (ensure(AttributeComp) && !AttributeComp->IsFullHealth())
+	{
+		// Only activate if healed successfully
+		if (AttributeComp->ApplyHealthChange(this, AttributeComp->GetHealthMax()))
 		{
-			if (AttributeComp->ApplyHealthChange(InstigatorPawn, 20.0f))
-			{
-				Deactivate(); // if successful health change
-			}
+			HideAndCooldownPowerup();
 		}
 	}
 }
-
--- Source/ActionRogueLike/Public/SHealthPotion.h
+++ Source/ActionRogueLike/Public/SHealthPotion.h
@@ -3,44 +3,24 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
-#include "SGameplayInterface.h"
+#include "SPowerupActor.h"
 #include "SHealthPotion.generated.h"
 
-class USphereComponent;
 class UStaticMeshComponent;
 
 UCLASS()
-class ACTIONROGUELIKE_API ASHealthPotion : public AActor, public ISGameplayInterface
+class ACTIONROGUELIKE_API ASHealthPotion : public ASPowerupActor
 {
 	GENERATED_BODY()
 	
-public:	
-	// Sets default values for this actor's properties
-	ASHealthPotion();
-
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-	UPROPERTY(EditAnywhere)
-	float RespawnTime;
-
-	UPROPERTY(VisibleAnywhere, Category = "Components")
-	USphereComponent* SphereComp;
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UStaticMeshComponent* MeshComp;
 
-	UFUNCTION()
-	void Activate();
-
-	void Deactivate();
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+public:
 
 	void Interact_Implementation(APawn* InstigatorPawn) override;
 
+	ASHealthPotion();
 };
