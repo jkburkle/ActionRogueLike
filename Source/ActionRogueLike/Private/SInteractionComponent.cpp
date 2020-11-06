bff1c997369a487f2f34c8ba19d6f240f01178fd
June Burkle <jonathan.k.burkle@gmail.com>
2020-11-06 00:21:35 -0500

    Add lecture 19 content
     - (there is no chest)

--- Source/ActionRogueLike/Private/SInteractionComponent.cpp
+++ Source/ActionRogueLike/Private/SInteractionComponent.cpp
@@ -37,7 +37,11 @@ void USInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickType,
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	FindBestInteractable();
+	APawn* MyPawn = Cast<APawn>(GetOwner());
+	if (MyPawn->IsLocallyControlled())
+	{
+		FindBestInteractable();
+	}
 }
 
 void USInteractionComponent::FindBestInteractable()
@@ -119,7 +123,12 @@ void USInteractionComponent::FindBestInteractable()
 
 void USInteractionComponent::PrimaryInteract()
 {
-	if (FocusedActor == nullptr)
+	ServerInteract(FocusedActor);
+}
+
+void USInteractionComponent::ServerInteract_Implementation(AActor* InFocus)
+{
+	if (InFocus == nullptr)
 	{
 		GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Red, "No focus actor to interact.");
 		return;
@@ -127,6 +136,6 @@ void USInteractionComponent::PrimaryInteract()
 	
 	APawn* MyPawn = Cast<APawn>(GetOwner());
 			
-	ISGameplayInterface::Execute_Interact(FocusedActor, MyPawn);
+	ISGameplayInterface::Execute_Interact(InFocus, MyPawn);
 }
 
